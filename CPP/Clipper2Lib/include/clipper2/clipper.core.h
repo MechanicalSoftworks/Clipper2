0c5e34f153f65284d648785b0b6829a13c674e56
Lucas Zadrozny <lucas.zadrozny@hotmail.com>
2025-06-20 18:49:35 +1000

    Remainder of GCC 14 fixes

--- CPP/Clipper2Lib/include/clipper2/clipper.core.h
+++ CPP/Clipper2Lib/include/clipper2/clipper.core.h
@@ -120,7 +120,7 @@ namespace Clipper2Lib
     }
 
     template <typename T2>
-    explicit Point<T>(const Point<T2>& p)
+    explicit Point(const Point<T2>& p)
     {
       Init(p.x, p.y, p.z);
     }
@@ -160,7 +160,7 @@ namespace Clipper2Lib
     template <typename T2>
     Point(const T2 x_, const T2 y_) { Init(x_, y_); }
 
-    template<typename T2>
+    template <typename T2>
     explicit Point(const Point<T2>& p) { Init(p.x, p.y); }
 
     Point operator * (const double scale) const
